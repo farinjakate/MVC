871f2bdc5919b932a4320d654482ecc97e92227e
Farin Jakate <150505333+farinjakate@users.noreply.github.com>
2024-08-05 10:36:51 +0530

    Add files via upload

--- Global.asax.cs
+++ Global.asax.cs
@@ -5,7 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
 
-namespace _2_MVCInViews
+namespace _3_MVC_CRUD_UsingEFDBFirst
 {
     public class MvcApplication : System.Web.HttpApplication
     {
